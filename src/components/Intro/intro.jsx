9ae704cbb008ae51f646a974b0c773c5c0577bbf
Simen Bergo <simenbergo@Simens-MacBook-Air.local>
2022-03-03 21:36:32 +0100

    image

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './intro.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import me from '../../assets/me.jpg'
 
 function Intro() {
 
@@ -9,8 +10,10 @@ function Intro() {
 
   return (
     <div id="intro" data-aos="fade-right">
+        <img src={me} alt='picture of Simen Bergo' />
         <h2>Hi, im Simen Bergo</h2>
         <p>A full-stack web developer from Norway</p>
+        <p>Currently writing my bachelors thesis at NTNU Gjøvik</p>
     </div>
   )
 }
