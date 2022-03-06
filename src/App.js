6263819ee81d6c150b8728618314d60c046b709b
Simen Bergo <bergo.simen@gmail.com>
2022-03-06 22:01:12 +0100

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Footer, Bio, Projects, Navbar, Intro, Scroll } from './components';
+import Div100vh from 'react-div-100vh';
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
+      <Div100vh>
         <div id="content">
         <div className='gradient__bg'>
           <Navbar />
@@ -12,8 +14,8 @@ const App = () => {
         <Intro />
         <Scroll />
         </div>
+        </Div100vh>
         <Bio />
-        
         <Projects />
         <Footer />
         <p id="copyright">© 2022 Built by Simen Tokerød Bergo</p>
