7fdabcd83b379531a77fd909e3a0bb3d0145b2b8
Simen Bergo <bergo.simen@gmail.com>
2022-03-10 18:22:48 +0100

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { Footer, Bio, Projects, Navbar, Intro, Scroll } from './components';
 import Div100vh from 'react-div-100vh';
+import useLocalStorage from 'use-local-storage'
+import { RiSunLine, RiMoonLine } from "react-icons/ri";
 import './App.css';
 
 const App = () => {
+
+  console.log('Hello there');
+
+  /* const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const Toggle = () => (
+    <div className="theme-switch-wrapper">
+        <label className="theme-switch" htmlFor="checkbox">
+            <input type="checkbox" id="checkbox" onClick={switchTheme}/>
+            <div className="slider round">
+            {theme === 'light' ? 
+            (<RiSunLine id="sun" size={40} />) : 
+            (<RiMoonLine id="moon" size={40}/>)}
+          </div>
+        </label>
+    </div>
+  )
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+} */
+
   return (
-    <div className='App'>
+    <div className='App' /* data-theme={theme} */>
       <Div100vh>
         <div id="content">
         <div className='gradient__bg'>
           <Navbar />
+          {/* <Toggle /> */}
         </div>
         <Intro />
         <Scroll />
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
   <footer>
       <p>My links: </p>
       <div id="links">
-      <a href="https://github.com/SimenBergo" target="_blank" rel="noreferrer"><SocialIcon url="https://github.com/SimenBergo" fgColor="#BB3434" bgColor="#000000"/></a>
-      <a href="https://www.linkedin.com/in/simen-toker%C3%B8d-bergo-301491227/" target="_blank" rel="noreferrer"><SocialIcon url="https://www.linkedin.com/in/simen-toker%C3%B8d-bergo-301491227/" fgColor="#BB3434" bgColor="#000000"/></a>
-      <a href="mailto:bergo.simen@gmail.com" target="_blank" rel="noreferrer"><SocialIcon network="email" mail="mailto:bergo.simen@gmail.com" fgColor="#BB3434" bgColor="#000000"/></a>
+      <SocialIcon url="https://github.com/SimenBergo" target="_blank" rel="noreferrer" fgColor="#BB3434" bgColor="#000000"/>
+      <SocialIcon url="https://www.linkedin.com/in/simen-toker%C3%B8d-bergo-301491227/" target="_blank" rel="noreferrer" fgColor="#BB3434" bgColor="#000000"/>
+      <SocialIcon network="email" mail="mailto:bergo.simen@gmail.com" target="_blank" rel="noreferrer" fgColor="#BB3434" bgColor="#000000"/>
       </div>
       
   </footer>
