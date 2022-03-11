fa77aada12414ce44fb6b7d674bf9e82ca4067fa
Simen Bergo <bergo.simen@gmail.com>
2022-03-11 10:19:52 +0100

    dark mode 2

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
       <div id="links">
       <SocialIcon url="https://github.com/SimenBergo" target="_blank" rel="noreferrer" fgColor="#BB3434" bgColor="#000000"/>
       <SocialIcon url="https://www.linkedin.com/in/simen-toker%C3%B8d-bergo-301491227/" target="_blank" rel="noreferrer" fgColor="#BB3434" bgColor="#000000"/>
-      <SocialIcon network="email" mail="mailto:bergo.simen@gmail.com" target="_blank" rel="noreferrer" fgColor="#BB3434" bgColor="#000000"/>
+      <SocialIcon url="mailto:bergo.simen@gmail.com" network="email" target="_blank" rel="noreferrer" fgColor="#BB3434" bgColor="#000000"/>
       </div>
       
   </footer>
